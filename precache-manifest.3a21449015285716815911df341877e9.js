050272040edd8866664ec3fed34f44a67213a908
Bryan <brymso@gmail.com>
2021-06-07 15:30:30 -0500

    Updates

---
+++ precache-manifest.3a21449015285716815911df341877e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0932c44993535d76b05c41e870194d41",
+    "url": "/Sorting-Algorithm-Visualizer/index.html"
+  },
+  {
+    "revision": "2c6f98dc816349980d66",
+    "url": "/Sorting-Algorithm-Visualizer/static/css/main.c10807eb.chunk.css"
+  },
+  {
+    "revision": "380c44d2a2d28b4d21cf",
+    "url": "/Sorting-Algorithm-Visualizer/static/js/2.fce4aa42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Sorting-Algorithm-Visualizer/static/js/2.fce4aa42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c6f98dc816349980d66",
+    "url": "/Sorting-Algorithm-Visualizer/static/js/main.405948d3.chunk.js"
+  },
+  {
+    "revision": "f86e7d958c7168f0023e",
+    "url": "/Sorting-Algorithm-Visualizer/static/js/runtime-main.8385d604.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/BryanSolomon/Sorting-Algorithm-Visualizer/precache-manifest.b78455e17d1211166cf9afbb6b070e82.js"
+  "/Sorting-Algorithm-Visualizer/precache-manifest.3a21449015285716815911df341877e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BryanSolomon/Sorting-Algorithm-Visualizer/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Sorting-Algorithm-Visualizer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
